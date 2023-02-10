4254eda33dc3d3088307e14266a45103551b1bd3
曾智麟 <>
2023-02-10 22:46:30 +0800

    前端主题框架搭建

--- vue project/vue/src/main.js
+++ vue project/vue/src/main.js
@@ -4,10 +4,11 @@ import router from './router'
 import store from './store'
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
+import './assets/gloable.css'
 
 Vue.config.productionTip = false
 
-Vue.use(ElementUI, {size:"small"})
+Vue.use(ElementUI)
 
 new Vue({
   router,
